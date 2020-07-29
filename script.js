1ff7c764481b2186641b441c0a22f7f9531381bd
dretekin <dretekin@gmail.com>
2020-07-29 17:04:41 +0000

    gsap integrated

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ let container = document.querySelector(".container"),
   containerCover = document.querySelector(".container-wrapper"),
   containerCoverDark = document.querySelector(".container-wrapperCover"),
   showNav = document.querySelector(".show-nav"),
-  nav = document.querySelector(".nav"),
   showSubNav = document.querySelectorAll(".nav-link_toSubNav"),
   subNav = document.querySelector(".sub-nav"),
   goBack = document.querySelectorAll(".nav-link_goBack");
@@ -17,51 +16,53 @@ let container = document.querySelector(".container"),
 //   document.body.clientWidth;
 // // let moveAmt = (80 / 100) * viewPortWidth original;
 let translateXNav = 0;
-let translateXCover = 0;
 let moveAmt = 300;
+let time = 0.8;
 
-showNav.addEventListener("click", function (e) {
+showNav.addEventListener("click", function () {
   translateXNav += moveAmt;
-  nav.style.transform = "translateX(" + translateXNav + "px)";
-  containerCover.style.transform = "translateX(" + moveAmt + "px)";
-  containerCover.addEventListener(
-    "transitionend",
-    function () {
+  gsap.to(".nav", { duration: time / 2, ease: "power1.out", x: translateXNav });
+  gsap.to(".container-wrapper", {
+    duration: time / 2,
+    ease: "power1.out",
+    x: moveAmt,
+    onComplete: function () {
       containerCoverDark.style.display = "block";
     },
-    { once: true }
-  );
+  });
 });
 
 containerCoverDark.addEventListener("click", function () {
   translateXNav -= moveAmt;
-  this.style.display = "none";
-  nav.style.transform = "translateX(" + translateXNav + "px)";
-  containerCover.style.transform = "translateX(0px)";
+  gsap.to(".nav", { duration: time / 2, ease: "power1.out", x: translateXNav });
+  gsap.to(".container-wrapper", {
+    duration: time / 2,
+    ease: "power1.out",
+    x: 0,
+    onComplete: () => {
+      this.style.display = "none";
+    },
+  });
 });
 
 showSubNav.forEach(function (subNav) {
-  subNav.addEventListener("click", function (e) {
+  subNav.addEventListener("click", function () {
     this.nextElementSibling.style.display = "block";
-
     translateXNav += moveAmt;
-    nav.style.transform = "translateX(" + translateXNav + "px)";
+    gsap.to(".nav", { duration: time, ease: "power1.out", x: translateXNav });
   });
 });
 
 goBack.forEach(function (el) {
-  el.addEventListener("click", function (e) {
-    nav.addEventListener(
-      "transitionend",
-      function hideSubNav() {
+  el.addEventListener("click", function () {
+    translateXNav -= moveAmt;
+    gsap.to(".nav", {
+      duration: time,
+      ease: "power1.out",
+      x: translateXNav,
+      onComplete: function () {
         el.parentElement.parentElement.style.display = "none";
       },
-      {
-        once: true,
-      }
-    );
-
-    translateXNav -= moveAmt;
-    nav.style.transform = "translateX(" + translateXNav + "px)";
+    });
   });
 });
